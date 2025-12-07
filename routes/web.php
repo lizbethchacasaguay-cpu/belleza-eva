e5e1a1c76bb3c2e81669683da713606c195ff632
lizbethchacasaguay-cpu <lizbethchacasaguay@gmail.com>
2025-12-06 19:45:47 -0500

    Estructura base del proyecto Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// RUTA DE PRUEBA TEMPORAL
+Route::get('/test-web', function() {
+    return response()->json(['message' => 'Ruta WEB funcionando correctamente']);
+});
